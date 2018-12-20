60db70e86c9458e6bc697ed6a4c79a47f2e539c7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-20 16:31:54 -0500

    made changes based on previous versions of similar projects

--- redactor.php
+++ redactor.php
@@ -1,6 +1,15 @@
 <?php
 
+//try changing this to get request
+$input = $_POST["pastedMessage"];
+
 function redactor($input){
     $nowArray = explode(' ', $input);
-    return $nowArray;
+    
+    foreach($nowArray as $word){
+        echo '<span>' . $word . '</span>';
+    }
 }
+redactor($input);
+
+?>
