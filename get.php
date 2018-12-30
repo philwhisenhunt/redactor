fac19b996a414c63845a50db181ff21a32d1bc42
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-29 20:46:21 -0500

    More from memory, not sure about echo

--- get.php
+++ get.php
@@ -3,5 +3,8 @@
 $receivedText = $_POST['theMessage'];
 
 function reverseIt($input){
-    
+    $reversed = strrev($input);
+    return $reversed;
 }
+
+reverseIt($receivedText); //is echo needed here?
